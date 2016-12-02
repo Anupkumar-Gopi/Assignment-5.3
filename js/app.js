b1eaaf5b8295efb2b58c08bbe8abc33cde4c2bb0
Anupkumar-Gopi <anupgopi.webdev@gmail.com>
2016-12-02 15:01:38 +0530

    Assignment-5.3

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var employee={
+	"name":"Anupkumar Gopi",
+	"age":18,
+	"salary":18000,
+	"address":{
+		"city":"Mumbai",
+		"state":"Maharashtra",
+		"pincode":400018
+	}
+};
+
+console.log("Name : "+employee.name+"\nAge : "+employee.age+"\nSalary : "+employee.salary+" (per month)\nCity : "+employee.address.city+"\nState : "+employee.address.state+"\nPincode : "+employee.address.pincode);
+
